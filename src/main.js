3668b9e3f176f9245da1ab83d3a0dd2324285011
Elijah Joseph-Young <eli.jy94@gmail.com>
2019-03-07 10:58:54 -0500

    Initial setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
-import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import store from './store';
+import App from './App.vue';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
