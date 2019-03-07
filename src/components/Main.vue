adabb7914487b44f4021e57b9eb17abe45d26a32
Elijah Joseph-Young <eli.jy94@gmail.com>
2019-03-07 13:29:59 -0500

    Get download button working

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <el-button type="danger" @click='download'>Download</el-button>
     <Search></Search>
     <Table></Table>
   </div>
@@ -14,6 +15,20 @@ export default {
   components: {
     Table,
     Search
+  },
+  computed: {
+    fileContent: function () {
+      return "data:text/csv;charset=utf-8," + this.$store.state.data.map(row => Object.values(row).map(item => `"${item}"`).join(",")).join("\r\n")
+    }
+  },
+  methods: {
+    download: function () {
+      const data = encodeURI(this.fileContent);
+      const link = document.createElement("a");
+      link.setAttribute("href", data);
+      link.setAttribute("download", "download.csv");
+      link.click();
+    }
   }
 };
 </script>
